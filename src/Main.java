57428ade95bee27540adc8721d32f081f5dcbaf8
YogeshKumar445 <ykyogesh445@gmail.com>
2026-01-20 09:37:50 +0530

    Main file added

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class BeatItMain extends JFrame {
+    public BeatItMain() {
+        setTitle("Beat It - 2D-Shooting game");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+
+        GamePanel panel = new GamePanel();
+        add(panel);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        panel.requestFocusInWindow();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new BeatItMain());
+    }
+}
+
+
+
+
+
+
+
